d176b193f0e7dcf1f4267737a72197454b37141c
Saman      keighobadi <61837740+SamanKeighobadi@users.noreply.github.com>
2022-07-22 17:44:05 +0430

    take screenshot and save iamge

--- app.js
+++ app.js
@@ -10,17 +10,26 @@ const streamVideo = async () => {
     video.srcObject = media;
 
     // add event
-    document.addEventListener("keypress",e => {
-        if(e.code !== "KeyP") reutrn ;
-        // create canvas element
-        const canvas = document.createElement('canvas')
-        canvas.width  = video.width;
-        canvas.height = video.height;
-        // Draw new image
-        canvas.getContext('2d').drawImage(video,0,0,video.width,video.height);
-        
-    })
+    document.addEventListener("keypress", (e) => {
+      if (e.code !== "KeyP") reutrn;
+      // create canvas element
+      const canvas = document.createElement("canvas");
+      canvas.width = video.width;
+      canvas.height = video.height;
+      // Draw new image
+      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
 
+      // Taks screen shot
+      let image = canvas.toDataURL("image/png").replace("image/png", 1.0);
+      picture.src = image;
+
+      // save image
+      const anchorTag = document.createElement("a");
+      anchorTag.href = image;
+      anchorTag.download = "screenshot.png";
+      document.body.appendChild(anchorTag);
+      anchorTag.click();
+    });
   } catch (ex) {
     console.log(ex);
   }
