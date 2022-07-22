57e9e3362fd1018722ad06a23770ffb05128e197
Saman      keighobadi <61837740+SamanKeighobadi@users.noreply.github.com>
2022-07-22 17:37:43 +0430

    create canvas element and draw image

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const streamVideo = async () => {
       video: true,
     });
     video.srcObject = media;
+
+    // add event
+    document.addEventListener("keypress",e => {
+        if(e.code !== "KeyP") reutrn ;
+        // create canvas element
+        const canvas = document.createElement('canvas')
+        canvas.width  = video.width;
+        canvas.height = video.height;
+        // Draw new image
+        canvas.getContext('2d').drawImage(video,0,0,video.width,video.height);
+        
+    })
+
   } catch (ex) {
     console.log(ex);
   }
